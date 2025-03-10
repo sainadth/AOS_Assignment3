3da14bc7e8682a87658bc6194757d124724548a6
sainadth <sainadthpagadala@gmail.com>
2025-03-10 02:41:11 -0500

    updated comments

--- pageFault.cpp
+++ pageFault.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 /**
  * @brief 
+ * randomly generates pages for the simulation
  * 
  * @param numPages 
  * @param maxPageNum 
@@ -21,10 +22,11 @@ vector<int> generatePages(int numPages, int maxPageNum) {
 /**
  * @brief FIFO Page Replacement Algorithm
  * 
- * If page found return true
+ * If page found return false
  * else
  *  - Remove the oldest page fomr the queue and set when pageSet is full
  *  - Add the page to the queue and set
+ *  - return true since it is a page fault
  * 
  * @param pageSet 
  * @param pageQueue 
@@ -35,6 +37,10 @@ vector<int> generatePages(int numPages, int maxPageNum) {
  */
 
 bool FIFO(unordered_set<int>& pageSet, queue<int>& pageQueue, int page, int capacity) {
+    /* 
+        pageSet stores distinct pages in the memory
+        pageQueue stores the order of pages in the memory
+    */
     if (pageSet.find(page) == pageSet.end()) {
         if (pageSet.size() >= capacity) {
             int oldestPage = pageQueue.front();
@@ -50,7 +56,8 @@ bool FIFO(unordered_set<int>& pageSet, queue<int>& pageQueue, int page, int capa
 
 /**
  * @brief LRU Page Replacement Algorithm
- * If page is found return true by pushing it to the front of the list
+ * If page is found return false by pushing it to the front of the list
+ * else push page to front and return true since it is a page fault
  * 
  * @param pageMap 
  * @param pageList 
@@ -61,8 +68,8 @@ bool FIFO(unordered_set<int>& pageSet, queue<int>& pageQueue, int page, int capa
  */
 bool LRU(unordered_map<int, list<int>::iterator>& pageMap, list<int>& pageList, int page, int capacity) {
     /* 
-        The pageMap stores the pageNumber and the iterator to the location of the page in the pageList. To efficiently handle the page repealcement of
-        the page.
+        The pageMap stores the pageNumber and the iterator to the latest location of the page in the pageList.
+        The pageList stores the order of pages in the memory
     */
     bool found = false;
     if (pageMap.find(page) == pageMap.end()) {
@@ -93,14 +100,17 @@ int main(){
         cout << i << " ";
     cout << endl;
 
-    //FIFO
+    //FIFO setup
     unordered_set<int> pageSet;
     queue<int> pageQueue; 
 
-    //LRU
+    //LRU setup
     unordered_map<int, list<int>::iterator> pageMap;
     list<int> pageList;
 
+    /* 
+        Initialize the memory with the generated pages for both LRU and FIFO
+    */
     for(auto i : pages){
         pageSet.insert(i);
         pageQueue.push(i);
@@ -114,14 +124,17 @@ int main(){
     int lru_page_faults = 0;
     
     // srand(time(0));
-    for(int hits = 1; hits < INT_MAX; hits *= 10){
-        for(int i = 0; i <= hits; i++){
+    /* 
+        page_access are increased by a factor of 10 each iteration
+    */
+    for(int page_access = 1; page_access < INT_MAX; page_access *= 10){
+        for(int i = 0; i <= page_access; i++){
             int page = rand() % (maxPageNum + 1);
             // cout << page << " ";
             if(FIFO(pageSet, pageQueue, page, capacity)) fifo_page_faults++;
             if(LRU(pageMap, pageList, page, capacity)) lru_page_faults++;
         }
-        cout << "\nNumber of Hits: " << hits << endl;
+        cout << "\nNumber of page_access: " << page_access << endl;
         cout << "Page Faults (FIFO): " << fifo_page_faults << endl;
         cout << "Page Faults (LRU): " << lru_page_faults << endl;
         // cout << pageSet.size() << " " << pageQueue.size() << " " << pageMap.size() << " " << pageList.size() << endl;
